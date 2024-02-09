2f7d0605056e6673f78718077442a7fd9fd72627
Atharva Nidhonkar <atharvanidhonkar10@gmail.com>
2024-02-09 15:10:20 +0530

    updated with ep1

---
+++ 01-Inception/App.js
@@ -0,0 +1,104 @@
+// creating nested structure through React
+/* 
+Structure-1
+    <div id="parent">
+        <div id="child">
+            <h1>I'm okay</h1>
+        </div>
+    </div>
+
+Structure-2
+      <div id="parent">
+        <div id="child">
+            <h1>I'm okay</h1>
+            <h1>I'm okay</h1>
+        </div>
+    </div>
+
+Structure-3
+      <div id="parent">
+        <div id="child">
+            <h1>I'm okay</h1>
+            <h1>I'm okay</h1>
+        </div>
+        <div id="child2">
+            <h1>I'm okay</h1>
+            <h1>I'm okay</h1>
+        </div>
+    </div>
+
+
+*/
+
+
+// Structure-1
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "I'm okay")
+//   )
+// );
+
+// console.log(parent);
+
+// Structure-2
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },[
+//       React.createElement("h1", {}, "I'm okay"),
+//       React.createElement("h2", {}, "I'm okay")
+//     ]
+    
+//   )
+// );
+
+// console.log(parent);
+
+
+// Structure-3
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child" },[
+        React.createElement("h1", {}, "I'm okay"),
+        React.createElement("h2", {}, "I'm okay")
+      ]
+      
+    ),
+    React.createElement(
+      "div",
+      { id: "child" },[
+        React.createElement("h1", {}, "I'm okay"),
+        React.createElement("h2", {}, "I'm okay")
+      ]
+      
+    )
+  ]
+);
+
+console.log(parent);
+
+
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+    xyz: "ancj",
+  },
+  "Hi this is me saying to React"
+);
+// this will take three parameters
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
